7c8e4d10843c36e6cbdefc427ca9c46afd6ae193
Howcanoe WANG <19968935+HowcanoeWang@users.noreply.github.com>
2023-07-16 17:12:11 +0900

    split bugreport from index.ts

---
+++ src/bugreportUI.ts
@@ -0,0 +1,30 @@
+import { Dialog } from "siyuan";
+import BgCoverPlugin from "./index"
+
+export function bugReportDialog(pluginInstance: BgCoverPlugin) {
+    const dialog = new Dialog({
+        title: `${pluginInstance.i18n.bugReportLabel}`,
+        content: `
+        <div class="b3-dialog__content">${pluginInstance.i18n.bugReportConfirmText}</div>
+        <div class="b3-dialog__action">
+            <button class="b3-button b3-button--cancel">${pluginInstance.i18n.cancel}</button><div class="fn__space"></div>
+            <button class="b3-button b3-button--text">${pluginInstance.i18n.confirm}</button>
+        </div>
+        <div class="b3-dialog__action">
+        `,
+        width: pluginInstance.isMobile ? "92vw" : `520px`,
+    });
+
+    const btnsElement = dialog.element.querySelectorAll(".b3-button");
+
+    // cancel button
+    btnsElement[0].addEventListener("click", () => {
+        dialog.destroy();
+    });
+
+    // still report
+    btnsElement[1].addEventListener("click", () => {
+        window.open('https://github.com/HowcanoeWang/siyuan-plugin-background-cover/issues', '_blank');
+        dialog.destroy();
+    });
+}
