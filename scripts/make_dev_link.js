763682c6449ebb9d2a054550bfaec85c68a8428e
unknown <howcanoewang@gmail.com>
2025-11-29 18:58:29 +0900

    build(vite): pass vite build pkg

--- scripts/make_dev_link.js
+++ scripts/make_dev_link.js
@@ -10,7 +10,7 @@
 import fs from 'fs';
 import { log, error, getSiYuanDir, chooseTarget, getThisPluginName, makeSymbolicLink } from './utils.js';
 
-let targetDir = '';
+let targetDir = 'C:/Users/hwang/Documents/SiYuanDev/data/plugins';
 
 /**
  * 1. Get the parent directory to install the plugin
